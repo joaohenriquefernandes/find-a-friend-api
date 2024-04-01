c4ddd0d40d266fbf6ae299f7576f65c372c11641
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-01 10:31:20 -0300

    feat: create orgs repository interface

---
+++ src/repositories/interfaces/IOrgsRepository.ts
@@ -0,0 +1,19 @@
+import { Org, Prisma } from '@prisma/client';
+
+export interface IOrgsRepository {
+  create: ({
+    author_name,
+    cep,
+    city,
+    email,
+    latitude,
+    longitude,
+    name,
+    neighborhood,
+    password,
+    state,
+    street,
+    whatsapp,
+  }: Prisma.OrgUncheckedCreateInput) => Promise<Org>;
+  findByEmail: (email: string) => Promise<Org | null>;
+}
