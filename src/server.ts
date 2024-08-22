b22be66d5b13e8c6b2e02c3b1ec8f42fcaea477b
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-08-22 08:59:33 -0300

    chore: initial configure project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Http Server Running at http://localhost:3333')
+  })
